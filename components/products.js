516deb6ba70eba31ee094f4f4f9fc09fb837f73c
Edgar Britez <edgarfbritez@gmail.com>
2022-03-15 09:50:35 -0300

    It will recive products by props

--- components/products.js
+++ components/products.js
@@ -1,26 +1,16 @@
 import Card from "./card";
-const fakeData = {
-  _id: "5a033f0f364bf301523e9b93",
-  name: "iPhone 7 Case Sea-Blue",
-  cost: 200,
-  category: "Accesorios",
-  img: {
-    url: "https://coding-challenge-api.aerolab.co/images/SamsungTabS2-x1.png",
-    hdUrl: "https://coding-challenge-api.aerolab.co/images/SamsungTabS2-x1.png",
-  },
-};
 
-export default function Products() {
+export default function Products({ products }) {
   return (
     <section>
-      <Card product={fakeData} />
-      <Card product={fakeData} />
-      <Card product={fakeData} />
-      <Card product={fakeData} />
+      {products.map((item) => (
+        <Card product={item} />
+      ))}
       <style jsx>{`
         section {
           display: flex;
           gap: 1em;
+          flex-wrap: wrap;
           max-width: 1176px;
 
           margin: 2rem auto;
