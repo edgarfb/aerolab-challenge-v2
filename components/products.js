be8dd3df550fb538ad7657a52d01f66e8c507f34
Edgar Britez <edgarfbritez@gmail.com>
2022-03-15 19:30:18 -0300

    added paginations

--- components/products.js
+++ components/products.js
@@ -1,19 +1,55 @@
+import { useState } from "react";
 import Card from "./card";
+import { useProductsContext } from "../context/ProductsContext";
 
-export default function Products({ products }) {
+export default function Products() {
+  const [initPage, setInitPage] = useState(0);
+  const [endPage, setEndPage] = useState(16);
+  const products = useProductsContext();
   return (
     <section>
-      {products.map((item) => (
-        <Card product={item} />
-      ))}
+      <div className="btn-pagination">
+        <button
+          onClick={() => {
+            if (initPage === 0) return;
+
+            setInitPage(initPage - 16);
+            setEndPage(endPage - 16);
+          }}
+        >
+          &lt;
+        </button>
+        <button
+          onClick={() => {
+            if (endPage === products.length) return;
+
+            setInitPage(initPage + 16);
+            setEndPage(endPage + 16);
+          }}
+        >
+          &gt;
+        </button>
+      </div>
+      <div className="products-container">
+        {products
+          .map((item) => <Card key={item.name} product={item} />)
+          .slice(initPage, endPage)}
+      </div>
       <style jsx>{`
-        section {
+        .products-container {
           display: flex;
           gap: 1em;
           flex-wrap: wrap;
           max-width: 1176px;
 
           margin: 2rem auto;
+        }
+        .btn-pagination {
+          max-width: 1176px;
+          margin: 2rem auto;
+          display: flex;
+          justify-content: flex-end;
+          gap: 1em;
         }
       `}</style>
     </section>
