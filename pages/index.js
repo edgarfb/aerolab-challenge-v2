df77cb46b0ecd473343db4ad0ff3cd90585b8fe6
Edgar Britez <edgarfbritez@gmail.com>
2022-03-15 09:52:41 -0300

    testing getStaticProps(it's working), also I added a product-mock to work in dev env

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,36 @@ import Head from "next/head";
 import MainImage from "../components/main-image";
 import ProductSorterBar from "../components/product-sorter";
 import Products from "../components/products";
+import { useAppContext } from "../context/appContext";
+import { productsMock } from "../fake-data/products-mock";
 
-export default function Home() {
+export default function Home({ products }) {
+  console.log("productsMock", productsMock);
   return (
     <>
       <MainImage />
       <ProductSorterBar />
-      <Products />
+      {/* <Products products={products} /> */}
+      <Products products={productsMock} />
     </>
   );
 }
+
+// for production build
+// export async function getStaticProps() {
+//   const res = await fetch("https://coding-challenge-api.aerolab.co/products", {
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//       Authorization:
+//         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjJjN2Q3MGIyNWRhMDAwMjE4NjU0NmEiLCJpYXQiOjE2NDcwODI4NjR9.gToydF6wdHHAOr8_2v0sc3K7c_U9EihcuWNVB4x6Eds",
+//     },
+//   });
+//   const products = await res.json();
+
+//   return {
+//     props: {
+//       products,
+//     },
+//   };
+// }
