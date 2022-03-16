aa611c0b24ff0358f2bb7910402269af103de973
Edgar Britez <edgarfbritez@gmail.com>
2022-03-15 23:07:57 -0300

    hover just on products that have enoght points and a message if the product does not have enough points

--- components/card.js
+++ components/card.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import { useState } from "react";
 import CardOverlay from "./card-overlay";
+import { useUserContext } from "../context/UserContext";
 
 export default function Card({ product }) {
+  const user = useUserContext();
   const [isHover, setIsHover] = useState(false);
   return (
     <div
@@ -10,15 +12,33 @@ export default function Card({ product }) {
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
     >
-      {isHover && <CardOverlay product={product} />}
-      <div className="buy-bag">
-        <Image
-          src="/images/icons/buy-blue.svg"
-          alt=""
-          width="30px"
-          height="30px"
-        />
-      </div>
+      {/* The overlay card will be display only if the product cost is less or equal to users points */}
+      {isHover && user.points > product.cost && (
+        <CardOverlay product={product} />
+      )}
+      {user.points > product.cost && (
+        <div className="buy-bag">
+          <Image
+            src="/images/icons/buy-blue.svg"
+            alt=""
+            width="30px"
+            height="30px"
+          />
+        </div>
+      )}
+      {user.points < product.cost && (
+        <div className="not-enough-points">
+          <div>You need {product.cost}</div>
+          <div className="top-5" style={{ display: "flex" }}>
+            <Image
+              src="/images/icons/coin.svg"
+              alt=""
+              width="25px"
+              height="25px"
+            />
+          </div>
+        </div>
+      )}
       <div className="card-image">
         <Image
           src={product.img.url}
@@ -48,12 +68,25 @@ export default function Card({ product }) {
           position: absolute;
           top: 1rem;
           right: 1rem;
+          z-index: 10;
         }
 
         img {
           width: 100%;
         }
-        card-info {
+        .not-enough-points {
+          position: absolute;
+          top: 1rem;
+          right: 1rem;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-radius: 2rem;
+          color: var(--gray-very-light);
+          gap: 0.5rem;
+          padding: 0.5rem 1rem;
+          z-index: 10;
+          background-color: var(--gray-light);
         }
         .category {
           display: block;
