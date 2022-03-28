64842d49a1e32657aaa58ddd78edc1da79b9dafb
Edgar Britez <edgarfbritez@gmail.com>
2022-03-27 23:51:08 -0300

    working on redeem history page

--- pages/redeem-history.js
+++ pages/redeem-history.js
@@ -1,7 +1,51 @@
-export default function RedeemHistory() {
+import ProductsUserHistory from "../components/products-user-history";
+import { productsMock } from "../fake-data/products-mock";
+
+export default function RedeemHistory({ redeemHistory }) {
   return (
     <div>
-      <h1>Redeem History</h1>
+      {redeemHistory.length === 0 && (
+        <div className="redeem-history-empty">
+          <div className="redeem-history-empty-text">
+            There is no redeem history yet.
+          </div>
+        </div>
+      )}
+      {redeemHistory.length > 0 && (
+        <>
+          <h1>Redeem History</h1>
+          <ProductsUserHistory products={redeemHistory} />
+        </>
+      )}
+
+      <style jsx>{`
+        h1 {
+          text-align: center;
+        }
+        .redeem-history-empty {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: calc(100vh - 4rem);
+        }
+      `}</style>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const url = `${process.env.API_BASE_URL}/user/history`;
+  const res = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${process.env.TOKEN}`,
+    },
+  });
+  const products = await res.json();
+  return {
+    props: {
+      redeemHistory: products,
+    },
+  };
 }
