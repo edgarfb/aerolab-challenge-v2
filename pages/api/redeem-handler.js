d1182028dff0925a7e3f4fe0fbea9279df2d71ef
Edgar Britez <edgarfbritez@gmail.com>
2022-03-27 23:07:49 -0300

    Changed the redeem's logic

--- pages/api/redeem-handler.js
+++ pages/api/redeem-handler.js
@@ -1,11 +1,15 @@
-export default async function getUserHandler(req, res) {
-  const url = `${process.env.API_BASE_URL}/user/me`;
+export default async function redeemHandler(req, res) {
+  const url = `${process.env.API_BASE_URL}/redeem`;
   const data = await fetch(url, {
+    method: "post",
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
       Authorization: `Bearer ${process.env.TOKEN}`,
     },
+    body: JSON.stringify({
+      product_id: req.body.product_id,
+    }),
   });
   res.status(200).json(await data.json());
 }
