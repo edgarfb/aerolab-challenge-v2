bf18c317b1c4f6439c4c0dc11cb8b97b65a7a9d5
Edgar Britez <edgarfbritez@gmail.com>
2022-04-01 17:56:24 -0300

    redeem card action flow

--- components/card-overlay.js
+++ components/card-overlay.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { redeemProduct } from "../lib/redeem";
@@ -6,33 +6,64 @@ import { useUserDispatchContext } from "../context/UserContext";
 
 export default function CardOverlay({ product }) {
   const [isEnable, setIsEnable] = useState(true);
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [confirmRedeem, setConfirmRedeem] = useState(false);
   const router = useRouter();
 
   const dispatch = useUserDispatchContext();
+
   return (
     <div className="overlay-card">
       <div className="cost">
-        <span>{product.cost}</span>
-        <Image
-          className="top-5"
-          src="/images/icons/coin.svg"
-          alt=""
-          width="20px"
-          height="20px"
-        />
+        {!showConfirm && (
+          <>
+            <span>{product.cost}</span>
+            <Image
+              className="top-5"
+              src="/images/icons/coin.svg"
+              alt=""
+              width="20px"
+              height="20px"
+            />
+          </>
+        )}
+        {showConfirm && <span>Redeem this product?</span>}
       </div>
-      <button
-        className="btn-overlay-card"
-        disabled={!isEnable}
-        onClick={() => {
-          redeemProduct(product._id);
-          setIsEnable(false);
-          dispatch({ type: "DISCOUNT_POINTS", payload: product.cost });
-          router.push("/redeem-history");
-        }}
-      >
-        Redden now
-      </button>
+      {!showConfirm && (
+        <button
+          className="btn-overlay-card"
+          disabled={!isEnable}
+          onClick={() => {
+            setShowConfirm(true);
+          }}
+        >
+          Redden now
+        </button>
+      )}
+
+      {showConfirm && (
+        <div className="confirm-btn">
+          <button
+            onClick={() => {
+              setShowConfirm(false);
+              setConfirmRedeem(true);
+              redeemProduct(product._id);
+              dispatch({ type: "DISCOUNT_POINTS", payload: product.cost });
+              router.push("/redeem-history");
+            }}
+          >
+            Si
+          </button>
+          <button
+            onClick={() => {
+              setConfirmRedeem(false);
+              setShowConfirm(false);
+            }}
+          >
+            No
+          </button>
+        </div>
+      )}
 
       <div className="buy-bag-white">
         <Image
@@ -80,6 +111,25 @@ export default function CardOverlay({ product }) {
           position: absolute;
           top: 1rem;
           right: 1rem;
+        }
+
+        .confirm-btn {
+          display: flex;
+          justify-content: center;
+          gap: 1rem;
+          width: 60%;
+          margin: 0 auto;
+        }
+
+        .confirm-btn button {
+          border: none;
+          padding: 0.5rem 1rem;
+          border-radius: 1rem;
+        }
+        .confirm-btn button:hover {
+          cursor: pointer;
+          outline: 2px solid var(--gray-light);
+          // continue here
         }
       `}</style>
     </div>
